0e48c89591808fac9339d301d2b9e644764eb205
diarmuidmcgonagle <diarmuidmcgonagle@gmail.com>
2024-04-05 17:07:07 +0200

    add counter

---
+++ Sources/MarmitonUIMealzIOS/ui/General/MarmitonCounter.swift
@@ -0,0 +1,68 @@
+//
+//  MarmitonCounter.swift
+//
+//
+//  Created by Diarmuid McGonagle on 05/04/2024.
+//
+
+import SwiftUI
+import MealzIOSFramework
+
+@available (iOS 14, *)
+public struct MarmitonCounter<Content: View>: View {
+    let currentAmount: Int
+    let backgroundColor: Color
+    let buttonStrokeColor: Color
+    let cornerRadius: CGFloat
+    let content: () -> Content
+    let buttonAction: (Int) -> Void
+    
+    public init(
+        currentAmount: Int,
+        backgroundColor: Color = Color.clear,
+         buttonStrokeColor: Color = Color.clear,
+         cornerRadius: CGFloat = Dimension.sharedInstance.buttonCornerRadius,
+         content: @escaping () -> Content,
+         buttonAction: @escaping (Int) -> Void
+    ) {
+        self.currentAmount = currentAmount
+        self.backgroundColor = backgroundColor
+        self.buttonStrokeColor = buttonStrokeColor
+        self.cornerRadius = cornerRadius
+        self.content = content
+        self.buttonAction = buttonAction
+    }
+    
+    public var body: some View {
+        HStack(spacing: 0) {
+            Button {
+                buttonAction(max((currentAmount - 1), 1))
+            } label: {
+                Image.mealzIcon(icon: .minus)
+                    .resizable()
+                    .renderingMode(.template)
+                    .foregroundColor(Color.mealzColor(.primaryText))
+                    .frame(width: 20, height: 20)
+            }
+            .padding(Dimension.sharedInstance.mlPadding)
+            Divider().frame(maxHeight: .infinity)
+            content()
+            Divider().frame(maxHeight: .infinity)
+            Button {
+                buttonAction(currentAmount + 1)
+            } label: {
+                Image.mealzIcon(icon: .plus)
+                    .resizable()
+                    .renderingMode(.template)
+                    .foregroundColor(Color.mealzColor(.primaryText))
+                    .frame(width: 20, height: 20)
+            }
+            .padding(Dimension.sharedInstance.mlPadding)
+        }
+        .background(backgroundColor)
+        .cornerRadius(cornerRadius)
+        .overlay(RoundedRectangle(cornerRadius: cornerRadius)
+            .stroke(buttonStrokeColor, lineWidth: 1.0)
+        )
+    }
+}
--- Sources/MarmitonUIMealzIOS/ui/RecipeDetails/MarmitonRecipeDetailsHeaderView.swift
+++ Sources/MarmitonUIMealzIOS/ui/RecipeDetails/MarmitonRecipeDetailsHeaderView.swift
@@ -1,18 +1,42 @@
 //
 //  MarmitonRecipeDetailsHeaderView.swift
-//  
+//
 //
 //  Created by Diarmuid McGonagle on 04/04/2024.
 //
-
 
 import SwiftUI
 import MealzIOSFramework
 
 @available(iOS 14, *)
 public struct MarmitonRecipeDetailsHeaderView: RecipeDetailsHeaderProtocol {
-    public init() {}
+    let changeStore: () -> Void
+    public init(changeStore: @escaping () -> Void) {
+        self.changeStore = changeStore
+    }
     public func content(params: RecipeDetailsHeaderParameters) -> some View {
-        EmptyView()
+        VStack {
+            MarmitonChangeStoreButton(changeStore: changeStore)
+            MarmitonCounter(
+                currentAmount: params.currentGuests,
+                backgroundColor: Color.clear,
+                buttonStrokeColor: Color.mealzColor(.border),
+                content: {
+                    HStack {
+                        Spacer()
+                        Text("\(params.currentGuests)")
+                            .miamFontStyle(style: MiamFontStyleProvider.sharedInstance.bodyBoldStyle)
+                            .frame(minWidth: 10, alignment: .center)
+                            .foregroundColor(Color.mealzColor(.primary))
+                        Image.mealzIcon(icon: .cutlery)
+                            .resizable()
+                            .renderingMode(.template)
+                            .foregroundColor(Color.mealzColor(.primary))
+                            .frame(width: 15, height: 15)
+                        Spacer()
+                    }
+                }, buttonAction: params.onUpdateGuests)
+            .padding([.top, .horizontal], Dimension.sharedInstance.mlPadding)
+        }
     }
 }
