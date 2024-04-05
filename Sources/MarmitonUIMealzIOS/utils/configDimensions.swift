1020b631c35158a4ee350edb376626c9f465f1be
diarmuidmcgonagle <diarmuidmcgonagle@gmail.com>
2024-04-05 17:28:28 +0200

    add config dimensions

---
+++ Sources/MarmitonUIMealzIOS/utils/configDimensions.swift
@@ -0,0 +1,14 @@
+//
+//  configDimensions.swift
+//
+//
+//  Created by Diarmuid McGonagle on 05/04/2024.
+//
+
+import Foundation
+import MealzIOSFramework
+
+@available(iOS 14, *)
+public func configDimensions() {
+    Dimension.sharedInstance.buttonCornerRadius = 50
+}
