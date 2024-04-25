bba2f7d0240aa8c38eb0e105d1c060562130f738
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2024-04-25 09:34:55 +0100

    add footer that calls analytics basket.preview

---
+++ Sources/MarmitonUIMealzIOS/ui/RecipeDetails/MarmitonRecipeDetailsFooterView.swift
@@ -0,0 +1,39 @@
+//
+//  MarmitonRecipeDetailsFooterView.swift
+//
+//
+//  Created by miam x didi on 25/04/2024.
+//
+
+import SwiftUI
+import MealzIOSFramework
+import mealzcore
+import MealzUIModuleIOS
+
+@available(iOS 14, *)
+public struct MarmitonRecipeDetailsFooterView: RecipeDetailsFooterProtocol {
+    public init() {}
+    public func content(params: RecipeDetailsFooterParameters) -> some View {
+        // we will use the same UI View from Mealz Default, but overwrite the
+        // CTA so that it sends our analytics
+        MealzRecipeDetailsFooterView().content(params: RecipeDetailsFooterParameters(
+            totalPriceOfProductsAdded: params.totalPriceOfProductsAdded,
+            totalPriceOfProductsAddedPerGuest: params.totalPriceOfProductsAddedPerGuest,
+            totalPriceOfRemainingProducts: params.totalPriceOfRemainingProducts,
+            recipeStickerPrice: params.recipeStickerPrice,
+            numberOfGuests: params.numberOfGuests,
+            priceStatus: params.priceStatus,
+            ingredientsStatus: params.ingredientsStatus,
+            isAddingAllIngredients: params.isAddingAllIngredients,
+            cookOnlyMode: params.cookOnlyMode,
+            currentSelectedTab: params.currentSelectedTab,
+            callToAction: {
+                // only launch event when all products have been added or ignored
+                if params.ingredientsStatus.type == IngredientStatusTypes.noMoreToAdd {
+                    MealzDI.shared.analyticsService.sendEvent(eventType: Analytics.companion.EVENT_BASKET_PREVIEW, path: "", props: Analytics.setProps())
+                }
+                params.callToAction()
+            })
+        )
+    }
+}
