2f6c82de4001465a20b101f4541673dea7fc57f3
walerowicz-miam <149375102+walerowicz-miam@users.noreply.github.com>
2024-09-06 15:47:52 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -39,10 +39,10 @@ let package = Package(
             )
         } else {
             dependencies.append(contentsOf: [
-                .package(url: "https://github.com/miamtech/MealzCoreRelease", from: "4.1.0-alpha"),
-                .package(url: "https://github.com/miamtech/MealziOSSDKRelease", exact: "4.1.0-alpha"),
-                .package(url: "https://github.com/miamtech/MealzUIiOSSDKRelease", from: "1.1.0-alpha"),
-                .package(url: "https://github.com/miamtech/MealzNaviOSSDKRelease", from: "1.1.0-alpha4")
+                .package(url: "https://github.com/miamtech/MealzCoreRelease", from: "4.1.0"),
+                .package(url: "https://github.com/miamtech/MealziOSSDKRelease", exact: "4.1.0"),
+                .package(url: "https://github.com/miamtech/MealzUIiOSSDKRelease", from: "4.1.0"),
+                .package(url: "https://github.com/miamtech/MealzNaviOSSDKRelease", from: "4.1.0")
             ]
             )
         }
