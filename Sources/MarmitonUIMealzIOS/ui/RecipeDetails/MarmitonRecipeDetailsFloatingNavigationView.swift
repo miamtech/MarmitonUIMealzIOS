b3e86758dd349a7636be5b18f1068159f1a6baba
walerowicz-miam <149375102+walerowicz-miam@users.noreply.github.com>
2025-01-09 18:39:06 +0100

    update floating header back and title button

--- Sources/MarmitonUIMealzIOS/ui/RecipeDetails/MarmitonRecipeDetailsFloatingNavigationView.swift
+++ Sources/MarmitonUIMealzIOS/ui/RecipeDetails/MarmitonRecipeDetailsFloatingNavigationView.swift
@@ -5,13 +5,17 @@
 //  Created by didi on 22/10/2024.
 //
 
-import SwiftUI
 import MealziOSSDK
+import SwiftUI
 
 @available(iOS 14, *)
 public struct MarmitonRecipeDetailsFloatingNavigationView: RecipeDetailsFloatingNavigationProtocol {
+    public var isEmpty: Bool {
+        return true
+    }
+
     public init() {}
     public func content(params: RecipeDetailsFloatingNavigationParameters) -> some View {
-       EmptyView()
+        EmptyView()
     }
 }
