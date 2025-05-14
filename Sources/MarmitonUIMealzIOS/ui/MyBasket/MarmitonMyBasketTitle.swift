251ed119440b0ff60a712da0d5fc865398cb8fb8
Maxime Pezeril <maxime.pezeril@mealz.ai>
2025-05-14 11:37:35 +0200

    Use new Basket title protocol

--- Sources/MarmitonUIMealzIOS/ui/MyBasket/MarmitonMyBasketTitle.swift
+++ Sources/MarmitonUIMealzIOS/ui/MyBasket/MarmitonMyBasketTitle.swift
@@ -9,15 +9,15 @@ import SwiftUI
 import MealziOSSDK
 
 @available(iOS 14, *)
-public struct MarmitonMyBasketTitle: BaseTitleProtocol {
+public struct MarmitonMyBasketTitle: MyBasketTitleProtocol {
     let changeStore: () -> Void
     public init(changeStore: @escaping () -> Void) {
         self.changeStore = changeStore
     }
-    public func content(params: TitleParameters) -> some View {
+    public func content(params: MyBasketTitleParameters) -> some View {
         VStack {
             MarmitonChangeStoreButton(changeStore: changeStore)
-            MealzMyMealsTitle().content(params: params)
+            MealzMyBasketTitle().content(params: params)
         }
     }
 }
