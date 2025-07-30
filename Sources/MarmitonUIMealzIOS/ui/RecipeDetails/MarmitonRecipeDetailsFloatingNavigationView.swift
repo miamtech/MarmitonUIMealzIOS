ee6292537252b63cdb2b5c002ce66e01219675ac
Maxime Pezeril <maxime.pezeril@mealz.ai>
2025-07-30 16:09:07 +0200

    Fix/Add fixed navigation header to recipe details

--- Sources/MarmitonUIMealzIOS/ui/RecipeDetails/MarmitonRecipeDetailsFloatingNavigationView.swift
+++ Sources/MarmitonUIMealzIOS/ui/RecipeDetails/MarmitonRecipeDetailsFloatingNavigationView.swift
@@ -10,12 +10,24 @@ import SwiftUI
 
 @available(iOS 14, *)
 public struct MarmitonRecipeDetailsFloatingNavigationView: RecipeDetailsFloatingNavigationProtocol {
-    public var isEmpty: Bool {
-        return true
-    }
-
     public init() {}
-    public func content(params: RecipeDetailsFloatingNavigationParameters) -> some View {
-        EmptyView()
+
+    public func content(
+        params: RecipeDetailsFloatingNavigationParameters
+    ) -> some View {
+        HStack {
+            MealzRecipeDetailsFloatingNavigationView.closePage(onRecipeDetailsClosed: params.onRecipeDetailsClosed)
+                .padding(Dimension.sharedInstance.mPadding)
+            Spacer()
+            if let title = params.title {
+                Text(title)
+                    .lineLimit(2)
+                    .truncationMode(Text.TruncationMode.tail)
+                    .miamFontStyle(style: MiamFontStyleProvider.sharedInstance.titleStyle)
+                    .foregroundColor(Color.mealzColor(.standardDarkText))
+            }
+            Spacer()
+            MealzRecipeDetailsFloatingNavigationView.likeButton(recipeId: params.recipeId, isEnabled: params.isLikeEnabled, analyticsPath: params.analyticsPath)
+        }.background(Color.mealzColor(.white))
     }
 }
